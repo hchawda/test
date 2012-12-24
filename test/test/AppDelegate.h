4ee44de429085e7082a0dcd98296afbbeb46ac40
Hitesh <chawda.hitesh@fxbytes.com>
2012-12-24 16:52:59 +0530

    first commit

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Hitesh on 24/12/12.
+//  Copyright (c) 2012 Fxbytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ test/test/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  test
+//
+//  Created by Hitesh on 24/12/12.
+//  Copyright (c) 2012 Fxbytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
